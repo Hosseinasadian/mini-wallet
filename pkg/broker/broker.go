eff61194651d3b2920936c432d8b2bf140306b81
hossein <hosseinasadian442@gmail.com>
2026-05-20 18:59:05 +0330

    update pipline

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -43,4 +43,5 @@ type Config struct {
 	MaxRetry       int64         `koanf:"max_retry"`
 	PrefetchCount  int           `koanf:"prefetch_count"`
 	HandlerTimeout time.Duration `koanf:"handler_timeout"`
+	//
 }
